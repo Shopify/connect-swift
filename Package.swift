ba433a25c5e4ae708c0da80d28c1fab9df469c41
scottybobandy <scott@scottcampbell.dev>
2024-02-16 10:15:33 -0700

    Remove unused dependencies

--- Package.swift
+++ Package.swift
@@ -29,36 +29,12 @@ let package = Package(
             name: "Connect",
             targets: ["Connect"]
         ),
-        .library(
-            name: "ConnectMocks",
-            targets: ["ConnectMocks"]
-        ),
-        .library(
-            name: "ConnectNIO",
-            targets: ["ConnectNIO"]
-        ),
-        .executable(
-            name: "protoc-gen-connect-swift",
-            targets: ["ConnectSwiftPlugin"]
-        ),
-        .executable(
-            name: "protoc-gen-connect-swift-mocks",
-            targets: ["ConnectMocksPlugin"]
-        ),
+		.library(
+			name: "ConnectMocks",
+			targets: ["ConnectMocks"]
+		),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-nio.git",
-            from: "2.63.0"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-nio-http2.git",
-            from: "1.30.0"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-nio-ssl.git",
-            from: "2.26.0"
-        ),
         .package(
             url: "https://github.com/apple/swift-protobuf.git",
             from: "1.25.2"
@@ -77,93 +53,17 @@ let package = Package(
                 "README.md",
             ]
         ),
-        .executableTarget(
-            name: "ConnectConformanceClient",
-            dependencies: [
-                "Connect",
-                "ConnectNIO",
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            ],
-            path: "Tests/ConformanceClient",
-            exclude: [
-                "buf.gen.yaml",
-                "InvocationConfigs",
-                "README.md",
-            ]
-        ),
-        .testTarget(
-            name: "ConnectLibraryTests",
-            dependencies: [
-                "Connect",
-                "ConnectMocks",
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            ],
-            path: "Tests/UnitTests/ConnectLibraryTests",
-            exclude: [
-                "buf.gen.yaml",
-            ],
-            resources: [
-                .copy("TestResources"),
-            ]
-        ),
-        .target(
-            name: "ConnectMocks",
-            dependencies: [
-                .target(name: "Connect"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            ],
-            path: "Libraries/ConnectMocks",
-            exclude: [
-                "README.md",
-            ]
-        ),
-        .executableTarget(
-            name: "ConnectMocksPlugin",
-            dependencies: [
-                "ConnectPluginUtilities",
-                .product(name: "SwiftProtobufPluginLibrary", package: "swift-protobuf"),
-            ],
-            path: "Plugins/ConnectMocksPlugin"
-        ),
-        .target(
-            name: "ConnectNIO",
-            dependencies: [
-                "Connect",
-                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
-                .product(name: "NIOCore", package: "swift-nio"),
-                .product(name: "NIOFoundationCompat", package: "swift-nio"),
-                .product(name: "NIOHTTP1", package: "swift-nio"),
-                .product(name: "NIOHTTP2", package: "swift-nio-http2"),
-                .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "NIOSSL", package: "swift-nio-ssl"),
-            ],
-            path: "Libraries/ConnectNIO",
-            exclude: [
-                "README.md",
-            ]
-        ),
-        .target(
-            name: "ConnectPluginUtilities",
-            dependencies: [
-                .product(name: "SwiftProtobufPluginLibrary", package: "swift-protobuf"),
-            ],
-            path: "Plugins/ConnectPluginUtilities"
-        ),
-        .testTarget(
-            name: "ConnectPluginUtilitiesTests",
-            dependencies: [
-                "ConnectPluginUtilities",
-            ],
-            path: "Tests/UnitTests/ConnectPluginUtilitiesTests"
-        ),
-        .executableTarget(
-            name: "ConnectSwiftPlugin",
-            dependencies: [
-                "ConnectPluginUtilities",
-                .product(name: "SwiftProtobufPluginLibrary", package: "swift-protobuf"),
-            ],
-            path: "Plugins/ConnectSwiftPlugin"
-        ),
+		.target(
+			name: "ConnectMocks",
+			dependencies: [
+				.target(name: "Connect"),
+				.product(name: "SwiftProtobuf", package: "swift-protobuf"),
+			],
+			path: "Libraries/ConnectMocks",
+			exclude: [
+				"README.md",
+			]
+		),
     ],
     swiftLanguageVersions: [.v5]
 )
